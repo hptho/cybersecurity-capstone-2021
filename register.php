844e119875fb6b145273d0c5fe46f2d75518ab56
hptho <80236481+hptho@users.noreply.github.com>
2021-10-12 16:39:11 +0700

    Update register.php

--- register.php
+++ register.php
@@ -1,6 +1,6 @@
 <!-- Register a new user. -->
 <?php
-include('config.php');
+include ('config.php');
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -15,161 +15,170 @@ include('config.php');
 			<h1>Cybersecurity Capstone Project - October 2021</h1>
 		</div>
 <?php
-
 //We check if the form has been sent
-if(isset($_POST['username'], $_POST['password'], $_POST['passverif'], $_POST['email'], $_POST['maidenname'], $_POST['maidennamerepeat'], $_POST['elemschool'], $_POST['elemschoolrepeat'], $_POST['road'], $_POST['roadrepeat']) and $_POST['username'] != '')
+if (isset($_POST['username'], $_POST['password'], $_POST['passverif'], $_POST['email'], $_POST['maidenname'], $_POST['maidennamerepeat'], $_POST['elemschool'], $_POST['elemschoolrepeat'], $_POST['road'], $_POST['roadrepeat']) and $_POST['username'] != '')
 {
-	//We check if the two passwords are identical
-	$errors = [];
-	if ($_POST['maidenname'] != '' and $_POST['maidennamerepeat'] != '' and $_POST['elemschool'] != '' and $_POST['elemschoolrepeat'] != '' and  $_POST['road'] != '' and $_POST['roadrepeat'] != '')
-	{
-		if($_POST['password'] == $_POST['passverif'])
-		{		
-			//We check if the choosen password is strong enough.
-			if(checkPassword($_POST['password'], $errors))
-			{		
-				//We check if the email form is valid
-				if(preg_match('#^(([a-z0-9!\#$%&\\\'*+/=?^_`{|}~-]+\.?)*[a-z0-9!\#$%&\\\'*+/=?^_`{|}~-]+)@(([a-z0-9-_]+\.?)*[a-z0-9-_]+)\.[a-z]{2,}$#i',$_POST['email']))
-				{			
-					$username_input   = $_POST['username'];
-					$password_input   = $_POST['password'];
-					$email_input	  = $_POST['email'];
-					$maidenname_input = $_POST['maidenname'];
-					$elemschool_input = $_POST['elemschool'];				
-					$road_input	      = $_POST['road'];
-					
-					//Generate a five digit salt.				
-					$salt = (string)rand(10000, 99999);
-					
-					//Compute the hashes of salt concatenated to user data for sensitive information. 				
-					$password_input   = hash("sha512", $salt.$password_input);
-					$maidenname_input = hash("sha512", $salt.$maidenname_input);
-					$elemschool_input = hash("sha512", $salt.$elemschool_input);
-					$road_input       = hash("sha512", $salt.$road_input);
-					
-					// Check if user exists already in database
-					$stmt = $link->prepare("SELECT id FROM users WHERE username=?"); // prepare sql statement for execution
-					$stmt->bind_param("s", $username_input); // bind variables to the parameter markers of the prepared statement
-					$stmt->execute(); // executed prepared statement		
-					$result = $stmt->get_result(); // get result of executed statement
-					$stmt->close();
-					
-					if ($result != FALSE) 
-					{
-						/* determine number of rows result set */
-						$row_cnt = mysqli_num_rows($result);
-						mysqli_free_result($result);
-					}
-					else
-					{
-						echo "<script type=\"text/javascript\">alert(\"Last SQL query error: " . $link->error . "\")</script>";
-					}
-					
-					if($row_cnt == 0)
-					{
-						//We count the number of users to give an ID to this one
-						if ($result = $link->query('SELECT id FROM users')) 
-						{
-							/* determine number of rows result set */
-							$dn2 = mysqli_num_rows($result);
-							/* close result set */
-							mysqli_free_result($result);
-							$id = $dn2 + 1;
-						}
-						else
-						{
-							// show last error
-							echo "<script type=\"text/javascript\">alert(\"Last SQL query error: " . $link->error . "\")</script>";
-						}
-						
-						//Check if the password recovery questions are valid
-						$password_recovery_valid = false;
-						if (($_POST['maidenname'] == $_POST['maidennamerepeat']) and ($_POST['elemschool'] == $_POST['elemschoolrepeat']) and  ($_POST['road'] == $_POST['roadrepeat']))
-						{
-							$password_recovery_valid = true;
-						}
-						else
-						{
-							//Repeated answers to the password recovery questions are not always the same
-							$form	= true;
-							$message = 'The repeated answers to the password recovery questions are not always the same. Please try again and ensure the repeated answers are the same.';
-						}
-						
-						if ($password_recovery_valid == true)
-						{
-							// store user data in database
-							$stmt = $link->prepare("INSERT INTO users(id, username, password, email, maidenname, elemschool, road, signup_date, salt) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"); // prepare sql statement for execution
-							$stmt->bind_param("isssssssi", $id, $username_input, $password_input, $email_input, $maidenname_input, $elemschool_input, $road_input, $time, $salt); // bind variables to the parameter markers of the prepared statement
-							$time = time();
-							$result = $stmt->execute(); // executed prepared statement
-							$stmt->close();
-							if($result)
-							{
-								//We dont display the form
-								$form = false;
+    //We check if the two passwords are identical
+    $errors = [];
+    if ($_POST['maidenname'] != '' and $_POST['maidennamerepeat'] != '' and $_POST['elemschool'] != '' and $_POST['elemschoolrepeat'] != '' and $_POST['road'] != '' and $_POST['roadrepeat'] != '')
+    {
+        // We check if the username only letter and numberic.
+        if (preg_match('^[a-zA-Z0-9]*$', $_POST['username']))
+        {
+            if ($_POST['password'] == $_POST['passverif'])
+            {
+                //We check if the choosen password is strong enough.
+                if (checkPassword($_POST['password'], $errors))
+                {
+                    //We check if the email form is valid
+                    if (preg_match('#^(([a-z0-9!\#$%&\\\'*+/=?^_`{|}~-]+\.?)*[a-z0-9!\#$%&\\\'*+/=?^_`{|}~-]+)@(([a-z0-9-_]+\.?)*[a-z0-9-_]+)\.[a-z]{2,}$#i', $_POST['email']))
+                    {
+                        $username_input = $_POST['username'];
+                        $password_input = $_POST['password'];
+                        $email_input = $_POST['email'];
+                        $maidenname_input = $_POST['maidenname'];
+                        $elemschool_input = $_POST['elemschool'];
+                        $road_input = $_POST['road'];
 
-								echo "<div class=\"message\">You have successfuly been registered. You can log in now.<br />";
-								echo "<a href=\"access.php\">Login</a></div>";						
-							}
-							else
-							{
-								//Otherwise, we say that an error occured
-								$form	= true;
-								$message = 'An error occurred while signing up.';
-								echo "<script type=\"text/javascript\">alert(\"Last SQL query error: " . $link->error . "\")</script>";
-							}
-						}
-					}
-					else
-					{
-						//Otherwise, we say the username is not available
-						$form	= true;
-						$message = 'The username you want to use is not available, please choose another one.';
-					}
-				}
-				else
-				{
-					//Otherwise, we say the email is not valid
-					$form	= true;
-					$message = 'The email you entered is not valid.';
-				}
-			}
-			else
-			{
-				//Otherwise, we say the password is too weak
-				$form	= true;
-				$message = '';
-				foreach ($errors as $item)
-					$message = $message.$item."<BR>";
-			}
-		}
-		else
-		{
-			//Otherwise, we say the passwords are not identical
-			$form	 = true;
-			$message = 'The passwords you entered are not identical.';
-		}
-	}
-	else
-	{
-			//Security questions are empty
-			$form	 = true;
-			$message = 'One or more of the security questions are not answered, please answer them with words for security reasons.';		
-	}
+                        //Generate a five digit salt.
+                        $salt = (string)rand(10000, 99999);
+
+                        //Compute the hashes of salt concatenated to user data for sensitive information.
+                        $password_input = hash("sha512", $salt . $password_input);
+                        $maidenname_input = hash("sha512", $salt . $maidenname_input);
+                        $elemschool_input = hash("sha512", $salt . $elemschool_input);
+                        $road_input = hash("sha512", $salt . $road_input);
+
+                        // Check if user exists already in database
+                        $stmt = $link->prepare("SELECT id FROM users WHERE username=?"); // prepare sql statement for execution
+                        $stmt->bind_param("s", $username_input); // bind variables to the parameter markers of the prepared statement
+                        $stmt->execute(); // executed prepared statement
+                        $result = $stmt->get_result(); // get result of executed statement
+                        $stmt->close();
+
+                        if ($result != false)
+                        {
+                            /* determine number of rows result set */
+                            $row_cnt = mysqli_num_rows($result);
+                            mysqli_free_result($result);
+                        }
+                        else
+                        {
+                            echo "<script type=\"text/javascript\">alert(\"Last SQL query error: " . $link->error . "\")</script>";
+                        }
+
+                        if ($row_cnt == 0)
+                        {
+                            //We count the number of users to give an ID to this one
+                            if ($result = $link->query('SELECT id FROM users'))
+                            {
+                                /* determine number of rows result set */
+                                $dn2 = mysqli_num_rows($result);
+                                /* close result set */
+                                mysqli_free_result($result);
+                                $id = $dn2 + 1;
+                            }
+                            else
+                            {
+                                // show last error
+                                echo "<script type=\"text/javascript\">alert(\"Last SQL query error: " . $link->error . "\")</script>";
+                            }
+
+                            //Check if the password recovery questions are valid
+                            $password_recovery_valid = false;
+                            if (($_POST['maidenname'] == $_POST['maidennamerepeat']) and ($_POST['elemschool'] == $_POST['elemschoolrepeat']) and ($_POST['road'] == $_POST['roadrepeat']))
+                            {
+                                $password_recovery_valid = true;
+                            }
+                            else
+                            {
+                                //Repeated answers to the password recovery questions are not always the same
+                                $form = true;
+                                $message = 'The repeated answers to the password recovery questions are not always the same. Please try again and ensure the repeated answers are the same.';
+                            }
+
+                            if ($password_recovery_valid == true)
+                            {
+                                // store user data in database
+                                $stmt = $link->prepare("INSERT INTO users(id, username, password, email, maidenname, elemschool, road, signup_date, salt) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"); // prepare sql statement for execution
+                                $stmt->bind_param("isssssssi", $id, $username_input, $password_input, $email_input, $maidenname_input, $elemschool_input, $road_input, $time, $salt); // bind variables to the parameter markers of the prepared statement
+                                $time = time();
+                                $result = $stmt->execute(); // executed prepared statement
+                                $stmt->close();
+                                if ($result)
+                                {
+                                    //We dont display the form
+                                    $form = false;
+
+                                    echo "<div class=\"message\">You have successfuly been registered. You can log in now.<br />";
+                                    echo "<a href=\"access.php\">Login</a></div>";
+                                }
+                                else
+                                {
+                                    //Otherwise, we say that an error occured
+                                    $form = true;
+                                    $message = 'An error occurred while signing up.';
+                                    echo "<script type=\"text/javascript\">alert(\"Last SQL query error: " . $link->error . "\")</script>";
+                                }
+                            }
+                        }
+                        else
+                        {
+                            //Otherwise, we say the username is not available
+                            $form = true;
+                            $message = 'The username you want to use is not available, please choose another one.';
+                        }
+                    }
+                    else
+                    {
+                        //Otherwise, we say the email is not valid
+                        $form = true;
+                        $message = 'The email you entered is not valid.';
+                    }
+                }
+                else
+                {
+                    //Otherwise, we say the password is too weak
+                    $form = true;
+                    $message = '';
+                    foreach ($errors as $item) $message = $message . $item . "<BR>";
+                }
+            }
+            else
+            {
+                //Otherwise, we say the passwords are not identical
+                $form = true;
+                $message = 'The passwords you entered are not identical.';
+            }
+        }
+        else
+        {
+            //Otherwise, we say the username are not identical
+            $form = true;
+            $message = 'The username you entered are not identical. Only letter and numberic';
+
+        }
+    }
+    else
+    {
+        //Security questions are empty
+        $form = true;
+        $message = 'One or more of the security questions are not answered, please answer them with words for security reasons.';
+    }
 }
 else
 {
-	$form = true;
+    $form = true;
 }
 
-if ($form) 
+if ($form)
 {
-	//We display the form again
-	//We display a message if necessary
-	if(isset($message))
-	{
-		echo '<br><div class="message">'.$message.'</div>';
-	}
+    //We display the form again
+    //We display a message if necessary
+    if (isset($message))
+    {
+        echo '<br><div class="message">' . $message . '</div>';
+    }
 ?>
 		<div class="content">
 			<form action="register.php" method="post">
@@ -177,8 +186,14 @@ if ($form)
 				<p style="font-size:14px;color:Red;">(Password requires 8 characters minimum and must include at least one number, one lowercase letter, one uppercase letter and one symbol)</p><br />
 				<div class="center">
 					<p style="text-align:left;text-decoration: underline;">Enter user data:</p><br />
-					<label for="username" style="text-align:left;">Username</label><input type="text" name="username" value="<?php if(isset($_POST['username'])){echo htmlentities($_POST['username'], ENT_QUOTES, 'UTF-8');} ?>" required/><br />
-					<label for="email" style="text-align:left;">Email</label><input type="text" name="email" value="<?php if(isset($_POST['email'])){echo htmlentities($_POST['email'], ENT_QUOTES, 'UTF-8');} ?>" required /><br />
+					<label for="username" style="text-align:left;">Username</label><input type="text" name="username" value="<?php if (isset($_POST['username']))
+    {
+        echo htmlentities($_POST['username'], ENT_QUOTES, 'UTF-8');
+    } ?>" required/><br />
+					<label for="email" style="text-align:left;">Email</label><input type="text" name="email" value="<?php if (isset($_POST['email']))
+    {
+        echo htmlentities($_POST['email'], ENT_QUOTES, 'UTF-8');
+    } ?>" required /><br />
 					<label for="password" style="text-align:left;">Password</label><input type="password" name="password" required/><br />					
 					<label for="passverif" style="text-align:left;">Repeat Password</label><input type="password" name="passverif" required/><br /><br />
 					<p style="text-align:left;text-decoration: underline;">Answer the following questions, they are asked for in case you forget your password to setup a new one:</p><br />
@@ -193,7 +208,6 @@ if ($form)
 			</form>
 		</div>
 <?php
-
 }
 ?>
 		<div class="foot"><a href="<?php echo $url_home; ?>">Go to start page</a></div>
